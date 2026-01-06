ad4c2ea7c713341cacf3095879f01e74a3237ee6
Mehrab Farhadzadeh <mehrab7fm@gmail.com>
2026-01-06 17:50:58 +0330

    Init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Odin Weather App
+
+## Goals
+
+- Practicing working with API
+- Writing asynchronous code using promises and async/await
+
+## Not to Do
+
+- Spending unnecessary time on perfecting its beauty or structure, this is just a project to practice your knowledge, so you can move on as soon as it's *good enough*. That is, you're not supposed to work on your portfolio yet.
+
+## To Do
+
+- Showing some more data like humidity, feels like, etc.
---
+++ script.js
@@ -0,0 +1,77 @@
+function processData(data) {
+   return {
+      address: data.address,
+      currentTemp: data.currentConditions.temp,
+      currentCondition: data.currentConditions.conditions,
+   };
+}
+
+async function getGif(searchTerm) {
+   const API_KEY = "MYz5hujNezc7jKzm6F5zAX8N04cbGmcD";
+   const BASE_URL = "https://api.giphy.com/v1/gifs/translate";
+   const url = BASE_URL + "?api_key=" + API_KEY + "&s=" + searchTerm;
+   const response = await fetch(url);
+   const json = await response.json();
+   return json.data.images.original.url;
+}
+
+async function addToDom(processedData) {
+   const output = document.querySelector("output");
+   output.innerHTML = "";
+   const dataEl = document.createElement("p");
+   const imgEl = document.createElement("img");
+
+   if (processedData === undefined) {
+      dataEl.textContent = "Bad request!";
+      imgEl.src = await getGif("Bad request!");
+   } else {
+      dataEl.textContent =
+         processedData.address +
+         ": " +
+         processedData.currentTemp +
+         " " +
+         processedData.currentCondition;
+      // add gif src
+      imgEl.src = await getGif(processedData.currentCondition);
+   }
+   output.appendChild(dataEl);
+   output.append(imgEl);
+}
+
+function getWeather(location, unitGroup) {
+   if (!location) return;
+
+   const API_KEY = "YJSERCWKDFQ4WZPR5BAH4SQN9";
+   const BASE_URL =
+      "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+   const UNIT_GROUP = {
+      centigrade: "metric",
+      fahrenheit: "us",
+   };
+   const url =
+      BASE_URL +
+      location +
+      "?unitGroup=" +
+      UNIT_GROUP[unitGroup] +
+      "&key=" +
+      API_KEY +
+      "&contentType=json";
+   fetch(url)
+      .then((response) => {
+         if (!response.ok) throw new Error(response.status);
+         return response.json();
+      })
+      .then(processData)
+      .then(addToDom)
+      .catch((e) => addToDom());
+}
+
+function submitForm() {
+   const formEl = document.querySelector("form");
+   formEl.addEventListener("submit", (e) => {
+      e.preventDefault();
+      getWeather(e.target["location"].value, e.target["units"].value);
+   });
+}
+
+submitForm();
