f84bc6f914e11b67177ba980fbe7097e279db63b
Mehrab Farhadzadeh <mehrab7fm@gmail.com>
2026-01-07 14:17:03 +0330

    Add loading component

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function processData(data) {
 
 async function getGif(searchTerm) {
    const API_KEY = "MYz5hujNezc7jKzm6F5zAX8N04cbGmcD";
-   const BASE_URL = "https://api.giphy.com/v1/gifs/translate";
+   const BASE_URL = "https://api.giphy.com/v1/stickers/translate";
    const url = BASE_URL + "?api_key=" + API_KEY + "&s=" + searchTerm;
    try {
       const response = await fetch(url);
@@ -22,7 +22,6 @@ async function getGif(searchTerm) {
 
 async function addToDom(processedData) {
    const output = document.querySelector("output");
-   output.innerHTML = "";
    const dataEl = document.createElement("p");
    const imgEl = document.createElement("img");
 
@@ -39,6 +38,7 @@ async function addToDom(processedData) {
       // add gif src
       imgEl.src = await getGif(processedData.currentCondition);
    }
+   output.innerHTML = "";
    output.appendChild(dataEl);
    output.append(imgEl);
 }
@@ -75,6 +75,10 @@ function submitForm() {
    const formEl = document.querySelector("form");
    formEl.addEventListener("submit", (e) => {
       e.preventDefault();
+      document.querySelector("output p").textContent = "Loading...";
+      const LOADING_GIF_URL =
+         "https://media4.giphy.com/media/v1.Y2lkPWI5MzkyODBlNmY0cHo1bjJnaWtjcmcxYXduNTJ1cWc2b3owOW5kMWtib2F0bnEzYyZlcD12MV9zdGlja2Vyc190cmFuc2xhdGUmY3Q9cw/cvSf28B7CM3aPuCMDC/giphy.gif";
+      document.querySelector("output img").src = LOADING_GIF_URL;
       getWeather(e.target["location"].value, e.target["units"].value);
    });
 }
